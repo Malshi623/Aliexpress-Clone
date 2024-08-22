f8c3c4c7ae5053e3aa000dac0ef0f878710d2839
Sachini Malsha <59600001+Malshi623@users.noreply.github.com>
2024-08-22 08:46:55 +0000

    index page

--- vite-project/pages/index.vue
+++ vite-project/pages/index.vue
@@ -1,9 +1,12 @@
 <template>
-    <div class="bg-red-400">
-            TEST
-    </div>
+    <MainLayout>
+        <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">
+        </div>
+    </MainLayout>
 </template>
 
 <script setup>
+import MainLayout from '~/layouts/MainLayout.vue';
+
 
 </script>
